dda6bfc017ebd8dce595c7f9d98cee8c0b48529e
NISHABENIWA <138237902+NISHABENIWA@users.noreply.github.com>
2024-07-30 12:46:32 +0000

    summary

---
+++ .devcontainer/01_basics/03_dataconversion.js
@@ -0,0 +1,22 @@
+// primitive (call by value)
+// 7 type = string, number ,boolean , null, undefined, symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId)
+const bigNumber = 234567891234n
+
+//non primitive( call by refernce)
+// 3 type = array, object, function
+
+
+
